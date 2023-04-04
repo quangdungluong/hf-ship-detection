361bdb04a4ab53659ef85599aa5a63960c2c79bb
Luong Quang Dung <luongquangdung00@gmail.com>
2023-04-04 10:56:01 +0700

    update config

--- merge_csv.py
+++ merge_csv.py
@@ -6,11 +6,12 @@ from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
 from tqdm.auto import tqdm
 
-df1 = pd.read_csv("./data/submission_4fold.csv")
-df2 = pd.read_csv("./data/submission_fold4.csv")
+df1 = pd.read_csv("./data/submission_5fold.csv")
+df2 = pd.read_csv("./data/submission_4fold.csv")
+df3 = pd.read_csv("./data/submission_v7x_4fold.csv")
 
 labels = []
-for df in [df1, df2]:
+for df in [df1, df2, df3]:
     labels.append(df.label.values)
 images = sorted(glob.glob("../ship-detection/test/**/*.png", recursive=True))
 
@@ -19,7 +20,7 @@ for i, image_path in tqdm(enumerate(images)):
     img = Image.open(image_path)
     w, h = img.size
     list_predictions = [label[i] for label in labels]
-    final_results = merge_wbf(list_predictions, 0.55, h, w)
+    final_results = merge_wbf(list_predictions, 0.55, h, w, wbf_conf=0.01)
     merged_results.append(final_results)
 
 df = pd.DataFrame({
@@ -27,4 +28,4 @@ df = pd.DataFrame({
     "label": merged_results
 })
 
-df.to_csv("./data/m_5fold.csv", index=False)
+df.to_csv("./data/post_ensemble_v7x.csv", index=False)
